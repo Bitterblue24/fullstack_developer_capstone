fcc6400263e2e8881b72e0a91c91549e50086b04
“Bitterblue24” <taylorguasta24@gmail.com>
2025-08-27 14:19:08 -0400

    “postreviews”

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Dealers from './components/Dealers/Dealers';
 import Dealer from "./components/Dealers/Dealer";
 import PostReview from "./components/Dealers/PostReview";
-
+import Register from "./components/Register/Register"
 function App() {
   return (
     <Routes>
@@ -11,6 +11,7 @@ function App() {
         <Route path="/dealers" element={<Dealers/>} />
         <Route path="/dealer/:id" element={<Dealer/>} />
         <Route path="/postreview/:id" element={<PostReview/>} />
+        <Route path="/register" element={<Register/>} />
     </Routes>
   );
 }
