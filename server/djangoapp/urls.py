2cf32b3035c3bb08c4de889b96c2d67261fdb9bc
“Bitterblue24” <taylorguasta24@gmail.com>
2025-09-01 13:54:53 -0400

    “lintering”

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -3,23 +3,32 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LogoutView
 from .restapis import get_request, analyze_review_sentiments, post_review
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
+    # path for registration
+    path('register', view=views.registration, 
+    name='register'),
 
     # paths for login/logout
-    path(route='login', view=views.login_user, name='login'),
-    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
-    path('register', view=views.registration, name='register'),
-    
+    path(route='login', view=views.login_user, 
+    name='login'),
+    path('logout/', LogoutView.as_view(next_page='/'), 
+    name='logout'),
+
     #paths for retreiving data
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
-    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
-    path(route='add_review', view=views.add_review, name='add_review'),
+    path(route='get_cars', view=views.get_cars, 
+    name='getcars'),
+    path(route='get_dealers/', view=views.get_dealerships, 
+    name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, 
+    name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, 
+    name='dealer_details'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, 
+    name='dealer_details'),
+    path(route='add_review', view=views.add_review, 
+    name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
